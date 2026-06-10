f3c9522a01c9e871efaf43a02b84ab264cc4f31a
Ali Cicek <alicicek@kastamonu.edu.tr>
2026-06-10 14:12:30 +0300

    Fix SSH connection error Unknown DH group under BoringSSL/Electron by monkeypatching crypto.getDiffieHellman

--- server.js
+++ server.js
@@ -4,6 +4,29 @@ const { Server } = require('socket.io');
 const cors = require('cors');
 const ping = require('ping');
 const { SerialPort } = require('serialport');
+
+// Monkeypatch crypto.getDiffieHellman to support legacy DH groups (modp1, modp2, modp5)
+// on platforms where BoringSSL (bundled in Electron) does not support them natively.
+const crypto = require('crypto');
+const originalGetDiffieHellman = crypto.getDiffieHellman;
+crypto.getDiffieHellman = function(groupName) {
+  try {
+    return originalGetDiffieHellman.call(crypto, groupName);
+  } catch (err) {
+    if (err.message === 'Unknown DH group') {
+      const primes = {
+        modp1: 'ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a63a3620ffffffffffffffff',
+        modp2: 'ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece65381ffffffffffffffff',
+        modp5: 'ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffffffffffffffff'
+      };
+      if (primes[groupName]) {
+        return crypto.createDiffieHellman(Buffer.from(primes[groupName], 'hex'), Buffer.from([2]));
+      }
+    }
+    throw err;
+  }
+};
+
 const { Client } = require('ssh2');
 const wol = require('wake_on_lan');
 const os = require('os');
