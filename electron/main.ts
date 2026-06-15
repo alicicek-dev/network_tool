81682ecf1b2be147f8535100ef29ae2351653850
Ali Cicek <alicicek@kastamonu.edu.tr>
2026-06-15 11:43:36 +0300

    fix: change preload script extension from mjs to js to fix folder selection

--- electron/main.ts
+++ electron/main.ts
@@ -26,7 +26,7 @@ function createWindow() {
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.mjs'),
+      preload: path.join(__dirname, 'preload.js'),
       nodeIntegration: true,
       contextIsolation: true,
     },
